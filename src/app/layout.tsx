72fab0feb420ebcc6534c3232e039050dd7c03cd
Shashwat Mahendra <shashwatmahender2104@gmail.com>
2025-06-28 11:45:01 +0530

    chore: update dependencies to latest versions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,7 @@ export const metadata: Metadata = {
   description:
     'Our AI-driven platform transforms the recruitment process from job posting to candidate selection. Automate interviews, analyze resumes, and make data-driven hiring decisions.',
   icons: {
-    icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml', sizes: 'any', title: 'Developed by Shashwat' },
-    ],
+    icon: [{ url: '/favicon.svg', type: 'image/svg+xml', sizes: 'any' }],
     apple: '/images/favicon.svg',
   },
   authors: [{ name: 'Shashwat', url: 'https://github.com/Shashwat2104' }],
